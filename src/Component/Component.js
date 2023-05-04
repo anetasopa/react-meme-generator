f5eda9018101a880847fbe3d3ef35ce6765cd206
Aneta <sopaaneta@gmail.com>
2023-05-04 23:37:31 +0200

    Add top text

--- src/Component/Component.js
+++ src/Component/Component.js
@@ -6,7 +6,7 @@ import Header from './Header/Header';
 import Image from './Image/Image';
 
 function Component() {
-  const [topText, setTopText] = useState('');
+  const [topText, setTopText] = useState('Doge');
   const [bottomText, setBottomText] = useState('');
   const [template, setTemplate] = useState('doge');
   const [imageUrl, setImageUrl] = useState(
