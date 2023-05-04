1a803c3eaf6393cf1543b9a98d3a570d5d7e9139
Aneta <sopaaneta@gmail.com>
2023-05-04 23:17:45 +0200

    Fix the errors from drone

--- src/Component/Form/Inputs/Inputs.js
+++ src/Component/Form/Inputs/Inputs.js
@@ -4,7 +4,7 @@ function Inputs({ setTopText, setBottomText, topText, bottomText }) {
   return (
     <div className={styles.container}>
       <label htmlFor="topText">
-        Top Text:
+        Top text:
         <input
           placeholder="Top text"
           type="topText"
@@ -13,7 +13,7 @@ function Inputs({ setTopText, setBottomText, topText, bottomText }) {
         />
       </label>
       <label htmlFor="bottomText">
-        Bottom Text:
+        Bottom text:
         <input
           placeholder="Bottom text"
           type="bottomText"
