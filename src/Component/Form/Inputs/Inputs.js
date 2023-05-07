b20811a4c9914fb25aeebd26e2723d9269e1839b
Aneta <sopaaneta@gmail.com>
2023-05-07 11:41:00 +0200

    Add id to imputs

--- src/Component/Form/Inputs/Inputs.js
+++ src/Component/Form/Inputs/Inputs.js
@@ -3,22 +3,21 @@ import styles from './inputs.module.css';
 function Inputs({ setTopText, setBottomText, topText, bottomText }) {
   return (
     <div className={styles.container}>
-      <label>
-        Top text
-        <input
-          placeholder="Hello"
-          value={topText}
-          onChange={(e) => setTopText(e.target.value)}
-        />
-      </label>
-      <label>
-        Bottom text
-        <input
-          placeholder="World"
-          value={bottomText}
-          onChange={(e) => setBottomText(e.target.value)}
-        />
-      </label>
+      <label htmlFor="topText">Top text</label>
+      <input
+        placeholder="Hello"
+        id="topText"
+        name="topText"
+        value={topText}
+        onChange={(e) => setTopText(e.target.value)}
+      />
+      <label htmlFor="bottomText">Bottom text</label>
+      <input
+        placeholder="World"
+        id="bottomText"
+        value={bottomText}
+        onChange={(e) => setBottomText(e.target.value)}
+      />
     </div>
   );
 }
